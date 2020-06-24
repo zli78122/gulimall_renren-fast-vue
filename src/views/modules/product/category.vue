36802191795307c3c1bbd825c18b202cf40a55c6
zhengyuli <zhengyuli0321@gmail.com>
2020-06-24 00:48:02 -0700

    Product Category

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <el-tree :data="menus" :props="defaultProps"></el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      menus: [],
+      defaultProps: {
+        children: "children",
+        label: "name"
+      }
+    }
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get"
+      }).then(({ data }) => {
+        this.menus = data.data;
+      });
+    }
+  },
+  created() {
+    this.getMenus();
+  },
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {}
+};
+</script>
+
+<style scoped>
+
+</style>
