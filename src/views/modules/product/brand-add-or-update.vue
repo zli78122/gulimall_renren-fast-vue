268247979a2fbd2ed75eb0135f9f15a104cdada9
zhengyuli <zhengyuli0321@gmail.com>
2020-06-26 16:53:46 -0700

    Product Module

--- src/views/modules/product/brand-add-or-update.vue
+++ src/views/modules/product/brand-add-or-update.vue
@@ -31,7 +31,7 @@
         <el-input v-model="dataForm.firstLetter" placeholder="检索首字母"></el-input>
       </el-form-item>
       <el-form-item label="排序" prop="sort">
-        <el-input v-model="dataForm.sort" placeholder="排序"></el-input>
+        <el-input v-model.number="dataForm.sort" placeholder="排序"></el-input>
       </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
@@ -54,9 +54,9 @@ export default {
         name: "",
         logo: "",
         descript: "",
-        showStatus: "",
+        showStatus: 1,
         firstLetter: "",
-        sort: ""
+        sort: 0
       },
       dataRule: {
         name: [{ required: true, message: "品牌名不能为空", trigger: "blur" }],
@@ -92,7 +92,7 @@ export default {
             validator: (rule, value, callback) => {
               if (value === "") {
                 callback(new Error("排序字段必须填写"));
-              } else if (!/^\d+$/.test(value)) {
+              } else if (!Number.isInteger(value) || value < 0) {
                 callback(new Error("排序必须是一个大于等于0的整数"));
               } else {
                 callback();
