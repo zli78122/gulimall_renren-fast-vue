9f5f5c621f9975748d9282130bbf095bfd03873c
zhengyuli <zhengyuli0321@gmail.com>
2020-06-27 15:42:07 -0700

    Import PubSub

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV !== 'production') {
 // 挂载全局
 Vue.prototype.$http = httpRequest // ajax请求方法
 Vue.prototype.isAuth = isAuth     // 权限方法
-Vue.prototype.PubSub = PubSub     //组件发布订阅消息
+Vue.prototype.PubSub = PubSub     // 组件发布订阅消息
 
 // 保存整站vuex本地储存初始状态
 window.SITE_CONFIG['storeState'] = cloneDeep(store.state)
